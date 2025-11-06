cb0617875e4e12d0ba4f02e0fe39c4c90c38ae07
mkarampatsis <marka@central.ntua.gr>
2025-11-06 19:48:30 +0200

    Template forms

---
+++ src/app/components/step8-template-driven-forms/step8-template-driven-forms.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step8-template-driven-forms',
+  imports: [],
+  templateUrl: './step8-template-driven-forms.html',
+  styleUrl: './step8-template-driven-forms.css',
+})
+export class Step8TemplateDrivenForms {
+
+}
