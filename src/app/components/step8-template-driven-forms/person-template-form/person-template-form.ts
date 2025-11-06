47cc11b201111f002f161b8fe918e7569e75df27
mkarampatsis <marka@central.ntua.gr>
2025-11-06 20:34:37 +0200

    template driven forms

--- src/app/components/step8-template-driven-forms/person-template-form/person-template-form.ts
+++ src/app/components/step8-template-driven-forms/person-template-form/person-template-form.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { Person } from '../../../shared/interfaces/person';
 
 @Component({
   selector: 'app-person-template-form',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './person-template-form.html',
   styleUrl: './person-template-form.css',
 })
 export class PersonTemplateForm {
-
+  onSubmit(value:any){
+    console.log(value);
+  }
 }
