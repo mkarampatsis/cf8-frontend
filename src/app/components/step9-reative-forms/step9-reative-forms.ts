eef4a39ee971d05a1123def8a0452d3e0965d8a2
mkarampatsis <marka@central.ntua.gr>
2025-11-07 18:26:15 +0200

    Reactive forms

---
+++ src/app/components/step9-reative-forms/step9-reative-forms.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step9-reative-forms',
+  imports: [],
+  templateUrl: './step9-reative-forms.html',
+  styleUrl: './step9-reative-forms.css',
+})
+export class Step9ReativeForms {
+
+}
